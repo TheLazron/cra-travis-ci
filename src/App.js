f8cecf7b32ddc0abd5221b4b80234924f7ac4cbe
TheLazron <kaushik.rohan31@gmail.com>
2023-04-05 22:54:31 +0530

    master changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          learn react app
         </a>
       </header>
     </div>
