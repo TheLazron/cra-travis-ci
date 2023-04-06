3b5ffa24e901c232aa634642748015fa3051f574
TheLazron <kaushik.rohan31@gmail.com>
2023-04-07 01:42:30 +0530

    testing new pipeline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React App Docker Circle CI CD</p>
+        <p>React App </p>
         <a
           className="App-link"
           href="https://reactjs.org"
