c4ee5b55187ae79176462d7b38aa3e6987c7b42c
TheLazron <kaushik.rohan31@gmail.com>
2023-04-06 13:44:58 +0530

    testing deployment changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios.get("http://bit.ly/2mTM3nY").then((response) => {
+      console.log("got result");
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>polivcdjb</p>
+        <p>React App Docker Circle CI</p>
         <a
           className="App-link"
           href="https://reactjs.org"
