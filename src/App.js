9e3177177135368959b1111ad701316471b1e223
TheLazron <kaushik.rohan31@gmail.com>
2023-04-06 23:58:32 +0530

    feat changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>React App Docker Circle CI</p>
+        <p>React App Docker Circle CI CD</p>
         <a
           className="App-link"
           href="https://reactjs.org"
